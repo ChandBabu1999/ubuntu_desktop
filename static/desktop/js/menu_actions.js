95e486114d5a4ad568793e7a44cd7b434d447772
Dilip Sharma <dilipsharma640@gmail.com>
2020-06-07 15:05:34 +0530

    Add menu handler.

---
+++ static/desktop/js/menu_actions.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('#new_folder').click(function () {
+        $.post("/desktop/menu-action/", { action: 'new_folder' }, function (result) {
+            // do something with result
+        });
+    });
+
+    $('#new_doc').click(function () {
+        alert(2);
+    });
+});
